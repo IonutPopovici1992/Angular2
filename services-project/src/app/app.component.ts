50084638a85a556223d824f0afd04f9589d4fad1
Ionut Popovici <ipopovici@endava.net>
2017-04-28 18:31:46 +0300

    Added routes-project and services-project

---
+++ services-project/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ExampleService } from './example.service';
+
+@Component({
+	selector: 'app-root',
+	template: '<h1>{{ title }}</h1>'
+})
+
+export class AppComponent {
+
+	title: string;
+	
+	constructor(private _exampleService: ExampleService) {
+
+	}
+
+	ngOnInit() {
+		this.title = this._exampleService.someMethod();
+	}
+}
